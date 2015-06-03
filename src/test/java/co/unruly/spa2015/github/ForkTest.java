840f43be89bfc477f9fef134bab76b58cf9bd93a
Alex & Clive <unruly-oss@unrulymedia.com>
2015-06-03 13:55:51 +0000

    Java testing framework established

---
+++ src/test/java/co/unruly/spa2015/github/ForkTest.java
@@ -0,0 +1,14 @@
+package co.unruly.spa2015.github;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class ForkTest {
+
+    @Test
+    public void shouldHaveAWorkingTestFramework() throws Exception {
+        assertThat(false, is(true));
+    }
+}
