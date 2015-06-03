e1752564232078aa0c1ef29ce2ac3a308d41d087
Alex & Clive <unruly-oss@unrulymedia.com>
2015-06-03 13:59:27 +0000

    Java mocking framework established

--- src/test/java/co/unruly/spa2015/github/ForkTest.java
+++ src/test/java/co/unruly/spa2015/github/ForkTest.java
@@ -1,14 +1,21 @@
 package co.unruly.spa2015.github;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
+import java.util.List;
 
+import static org.mockito.Mockito.*;
+
+@RunWith(org.mockito.runners.MockitoJUnitRunner.class)
 public class ForkTest {
+
+    @Mock
+    private List<String> list;
 
     @Test
     public void shouldHaveAWorkingTestFramework() throws Exception {
-        assertThat(false, is(true));
+        verify(list).add("foo");
     }
 }
