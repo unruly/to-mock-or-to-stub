19005f69d48741c0c4e1aa3fb301bba3c860542c
Alex & Clive <unruly-oss@unrulymedia.com>
2015-06-03 13:41:56 +0000

    Ruby testing framework established

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'https://rubygems.org'
+
+gem 'octokit', '~> 3.8'
+
+gem 'rspec', '~>3.2', :group => :test
---
+++ spec/fork_spec.rb
@@ -0,0 +1,7 @@
+require 'rspec'
+
+describe 'GitHub forks' do
+  it 'should do something' do
+    expect(true).to be(false)
+  end
+end
